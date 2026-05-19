508dd6920d8ef9b012c4a381207468cb22c8b831
ERamirez20 <contact.estebanr@gmail.com>
2026-05-18 18:33:19 -0600

    modulo clients

--- FutureSkillCoachLite.Api/Program.cs
+++ FutureSkillCoachLite.Api/Program.cs
@@ -18,6 +18,9 @@
             builder.Services.AddScoped<ICoachRepository, CoachRepository>();
             builder.Services.AddScoped<ICoachService, CoachService>();
             builder.Services.AddScoped<ICoachFacade, CoachFacade>();
+            builder.Services.AddScoped<IClientRepository, ClientRepository>();
+            builder.Services.AddScoped<IClientService, ClientService>();
+            builder.Services.AddScoped<IClientFacade, ClientFacade>();
 
         // OpenAPI documentation.
         builder.Services.AddOpenApi();
